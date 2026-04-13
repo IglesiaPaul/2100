60d311edc79eb87ab184a7ac67bc9158b84ab393
SHIGHMAN <me@pauliglesia.com>
2026-04-13 16:15:57 +0200

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -3,11 +3,10 @@
     const navHtml = `
         <nav class="navbar">
             <div class="nav-container">
-                <a href="index.html" class="logo">🌱 SOLARPUNK FRANCE 🇫🇷</a>
+                <a href="index.html" class="logo">🌱 SOLARPUNK/France 🇫🇷</a>
                 <div class="nav-links">
                     <a href="index.html">Accueil</a>
-                    <a href="index.html#allMeasuresLink">Toutes les mesures</a>
-                    <a href="index.html#themesLink">Par thème</a>
+                    <a href="programme.html">📋 Le programme</a>
                     <a href="a-propos.html">À propos</a>
                     <a href="lab.html" style="font-weight: bold;">🔬 Lab</a>
                 </div>
