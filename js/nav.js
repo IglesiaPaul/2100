24032055b6181ef906bd6f0a94ad26d1d74b2ce7
SHIGHMAN <me@pauliglesia.com>
2026-04-13 15:36:21 +0200

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -3,14 +3,11 @@
     const navHtml = `
         <nav class="navbar">
             <div class="nav-container">
-                <a href="index.html" class="logo">🌱 SOLARPUNK 🇫🇷</a>
+                <a href="index.html" class="logo">🌱 SOLARPUNK FRANCE 🇫🇷</a>
                 <div class="nav-links">
                     <a href="index.html">Accueil</a>
                     <a href="index.html#allMeasuresLink">Toutes les mesures</a>
                     <a href="index.html#themesLink">Par thème</a>
-                    <a href="chatbot.html">🤖 Chatbot</a>
-                    <a href="quizz.html">📝 Quizz</a>
-                    <a href="https://lafranceinsoumise.fr/le-programme/" target="_blank">Le programme</a>
                     <a href="a-propos.html">À propos</a>
                     <a href="lab.html" style="font-weight: bold;">🔬 Lab</a>
                 </div>
